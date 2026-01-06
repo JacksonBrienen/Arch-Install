5ddec07d60aa5c499815e77521555d376ba51850
JacksonBrienen <jacksonbrienen@gmail.com>
2026-01-06 09:49:55 -0800

    More Bugs...

--- arch-install.sh
+++ arch-install.sh
@@ -4,12 +4,12 @@
 read -p "Enter hostname: " myhostname
 read -p "Enter username: " myusername
 read -sp "Enter password for $myusername: " mypassword
-echo """
+echo ""
 
 # Sync Clock
 timedatectl set-ntp true
 
-# GPT Partition - 512MB Boot, 4GB Swap, Rest as Root
+# GPT Partitioning - 512MB Boot, 4GB Swap, Rest as Root
 parted /dev/nvme0n1 mklabel gpt
 parted /dev/nvme0n1 mkpart "EFI" fat32 1MiB 513MiB
 parted /dev/nvme0n1 set 1 esp on
@@ -22,7 +22,6 @@ mkswap /dev/nvme0n1p2
 swapon /dev/nvme0n1p2
 mkfs.ext4 /dev/nvme0n1p3
 
-# Mount Boot and Root
 mount /dev/nvme0n1p3 /mnt
 mount --mkdir /dev/nvme0n1p1 /mnt/boot
 
@@ -33,70 +32,63 @@ pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode mesa vulkan-in
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Enter Installation
-arch-chroot /mnt <<EOF
+arch-chroot /mnt /bin/bash <<'EOF'
+# Re-import variables into the chroot environment
+export myhostname=$(cat /etc/hostname)
+# We need to pass the username and password explicitly since 'EOF' is quoted
+EOF
 
-# Timezone
+# Timezone, locale, and other environment variables
+cat <<SYSTEM_CONFIG > /mnt/setup.sh
+#!/bin/bash
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
-
-# Locale
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# Environment variables for Wayland/Intel
 echo "LIBVA_DRIVER_NAME=iHD" >> /etc/environment
 echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
-
-# Networking
 echo "$myhostname" > /etc/hostname
 systemctl enable NetworkManager
+systemctl enable sddm
 
 # User Management
 useradd -m -G wheel "$myusername"
 echo "$myusername:$mypassword" | chpasswd
-
-# Sudo Configuration
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-# Disable Root
 passwd -l root
 
-# Enable Login Manager
-systemctl enable sddm
-
-# Install Yay & Ghostty
-sudo -u "$myusername" bash <<AUR
-cd /home/"$myusername"
+# Yay & Ghostty
+sudo -u "$myusername" bash <<'AUR'
+cd /home/$(whoami)
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
 rm -rf yay
 yay -S --noconfirm ghostty
-
-# Pre-configure Hyprland to use Ghostty
-mkdir -p /home/"$myusername"/.config/hypr
-echo "\\$terminal = ghostty
-bind = SUPER, Q, exec, \\$terminal
+mkdir -p ~/.config/hypr
+echo "\$terminal = ghostty
+bind = SUPER, Q, exec, \$terminal
 bind = SUPER, M, exit,
-bind = SUPER, R, exec, wofi --show drun" > /home/"$myusername"/.config/hypr/hyprland.conf
-chown -R "$myusername":"$myusername" /home/"$myusername"/.config
+bind = SUPER, R, exec, wofi --show drun" > ~/.config/hypr/hyprland.conf
 AUR
 
-# Bootloader (systemd-boot)
+# Bootloader
 bootctl install
-
-# Create Bootloader Entry
+PARTUUID=\$(blkid -s PARTUUID -o value /dev/nvme0n1p3)
 echo "title   Arch Linux
 linux   /vmlinuz-linux
 initrd  /intel-ucode.img
 initrd  /initramfs-linux.img
-options root=PARTUUID=\$(blkid -s PARTUUID -o value /dev/nvme0n1p3) rw" > /boot/loader/entries/arch.conf
-
+options root=PARTUUID=\$PARTUUID rw" > /boot/loader/entries/arch.conf
 echo "default arch.conf
 timeout 3
 console-mode max" > /boot/loader/loader.conf
-EOF
+SYSTEM_CONFIG
+
+# Execute the setup script inside chroot
+arch-chroot /mnt /bin/bash /setup.sh
+rm /mnt/setup.sh
 
 echo "Installation complete! Unmount and reboot."
