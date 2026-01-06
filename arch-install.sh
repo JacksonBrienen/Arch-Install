df8ee582f180c31da4b4d7cc188f928b5bcc99f0
JacksonBrienen <jacksonbrienen@gmail.com>
2026-01-06 10:22:24 -0800

    .

--- arch-install.sh
+++ arch-install.sh
@@ -9,12 +9,15 @@ echo ""
 # --- 2. System Prep ---
 timedatectl set-ntp true
 
-# Partitioning /dev/nvme0n1 (GPT, 512MB Boot, 4GB Swap, Rest Root)
+# Partitioning
 parted /dev/nvme0n1 mklabel gpt
 parted /dev/nvme0n1 mkpart "EFI" fat32 1MiB 513MiB
 parted /dev/nvme0n1 set 1 esp on
 parted /dev/nvme0n1 mkpart "swap" linux-swap 513MiB 4609MiB
 parted /dev/nvme0n1 mkpart "root" ext4 4609MiB 100%
+
+# Force kernel to register new partitions
+udevadm settle
 
 # Formatting
 mkfs.fat -F 32 /dev/nvme0n1p1
@@ -27,74 +30,71 @@ mount /dev/nvme0n1p3 /mnt
 mount --mkdir /dev/nvme0n1p1 /mnt/boot
 
 # --- 3. Base Installation ---
-# Core + Intel 12th Gen Video + Hyprland + Bluetooth + Network Applets
 pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode mesa vulkan-intel \
 intel-media-driver sudo git vim networkmanager network-manager-applet \
 hyprland xdg-desktop-portal-hyprland waybar hyprpaper wofi qt5-wayland qt6-wayland \
-sddm bluez bluez-utils blueman
+sddm bluez bluez-utils blueman pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber
 
-# Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
+# Generate fstab (Clean rewrite to prevent UUID mismatch)
+udevadm settle
+genfstab -U /mnt > /mnt/etc/fstab
 
 # --- 4. Internal Setup Script ---
 cat <<INTERNAL_CONFIG > /mnt/setup.sh
 #!/bin/bash
 
-# System Clock & Locale
+# Locale/Time
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# Intel Media & Wayland variables
+# Env Vars
 echo "LIBVA_DRIVER_NAME=iHD" >> /etc/environment
 echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
 
-# Enable Services
+# Services
 echo "$myhostname" > /etc/hostname
 systemctl enable NetworkManager
 systemctl enable sddm
 systemctl enable bluetooth
 
-# User & Sudo Setup
+# User Setup
 useradd -m -G wheel "$myusername"
 echo "$myusername:$mypassword" | chpasswd
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 passwd -l root
 
-# Install Yay & Ghostty as User
+# AUR/Yay/Fonts/Ghostty
 sudo -u "$myusername" bash <<'AUR'
 export HOME=/home/"$myusername"
 cd /home/"$myusername"
-
 git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay
+cd yay && makepkg -si --noconfirm
+cd .. && rm -rf yay
 
-yay -S --noconfirm ghostty
+# Install Ghostty and Fonts
+# ttf-ms-fonts requires accepting a license, --noconfirm handles it.
+yay -S --noconfirm ghostty ttf-ms-fonts ttf-jetbrains-mono-nerd
 
-# Pre-configure Hyprland
 mkdir -p ~/.config/hypr
 echo "\\\$terminal = ghostty
-# Execute applets in background
 exec-once = nm-applet --indicator
 exec-once = blueman-applet
 exec-once = waybar
+exec-once = pipewire
+exec-once = wireplumber
 
-# Keybinds
 bind = SUPER, Q, exec, \\\$terminal
 bind = SUPER, M, exit,
 bind = SUPER, R, exec, wofi --show drun
 bind = SUPER, C, killactive," > ~/.config/hypr/hyprland.conf
 AUR
 
-# Final Permissions Fix
 chown -R "$myusername":"$myusername" /home/"$myusername"
 
-# Bootloader setup (systemd-boot)
+# Bootloader
 bootctl install
 UUID=\$(blkid -s PARTUUID -o value /dev/nvme0n1p3)
 echo "title   Arch Linux
@@ -102,10 +102,8 @@ linux   /vmlinuz-linux
 initrd  /intel-ucode.img
 initrd  /initramfs-linux.img
 options root=PARTUUID=\$UUID rw" > /boot/loader/entries/arch.conf
-
 echo "default arch.conf
-timeout 3
-console-mode max" > /boot/loader/loader.conf
+timeout 3" > /boot/loader/loader.conf
 INTERNAL_CONFIG
 
 # --- 5. Execution ---
@@ -114,4 +112,4 @@ arch-chroot /mnt /bin/bash /setup.sh
 rm /mnt/setup.sh
 
 echo "--------------------------------------------------"
-echo "Setup Complete! Type: umount -R /mnt && reboot"
+echo "Installation complete! Unmount and reboot."
